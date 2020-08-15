25caab7bddf1f7d4eb22018b723857e0c4de02ca
Javier Guajardo J <javier.guajardoj@gmail.com>
2020-08-15 00:04:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-selector = "#react-root > section > main > section > div.COOzN.MnWb5.YT6rB > div._8UZ6e > div.Igw0E.IwRSH.eGOV_._4EzTm.pjcA_.WKY0a > div > div > div > div:nth-child(5) > div.Igw0E.rBNOH.YBx95.ybXk5._4EzTm.soMvl > button > div"
-document.querySelector(selector).click()
+function click() {
+  selector = "#react-root > section > main > section > div.COOzN.MnWb5.YT6rB > div._8UZ6e > div.Igw0E.IwRSH.eGOV_._4EzTm.pjcA_.WKY0a > div > div > div > div:nth-child(5) > div.Igw0E.rBNOH.YBx95.ybXk5._4EzTm.soMvl > button > div"
+  document.querySelector(selector).click()
+}
